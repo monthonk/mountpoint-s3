f87fb99dbcb66abd8e9e2c60993f3a1c09576b19
Vlad Volodkin <vlaad@amazon.com>
2023-08-21 14:56:37 +0000

    Allow reading restored GFR/GDA objects (#434)
    Signed-off-by: Vlad Volodkin <vlaad@amazon.com>

--- mountpoint-s3-client/tests/head_object.rs
+++ mountpoint-s3-client/tests/head_object.rs
@@ -58,6 +58,7 @@ async fn test_head_object_storage_class(storage_class: &str) {
     assert_eq!(result.object.key, key);
     assert_eq!(result.object.size as usize, body.len());
     assert_eq!(result.object.storage_class.as_deref(), Some(storage_class));
+    assert!(!result.object.restored);
 }
 
 #[tokio::test]
@@ -104,4 +105,55 @@ async fn test_head_object_no_perm() {
         result,
         Err(ObjectClientError::ClientError(S3RequestError::Forbidden(_)))
     ));
+}
+
+// This test relies on s3's expedited object restoration, it takes 1-5 minutes to complete
+#[tokio::test]
+async fn test_head_object_restored() {
+    let sdk_client = get_test_sdk_client().await;
+    let (bucket, prefix) = get_test_bucket_and_prefix("test_head_object_restored");
+
+    let key = format!("{prefix}/hello");
+    let body = b"hello world!";
+    sdk_client
+        .put_object()
+        .bucket(&bucket)
+        .key(&key)
+        .storage_class("GLACIER".into())
+        .body(ByteStream::from(Bytes::from_static(body)))
+        .send()
+        .await
+        .unwrap();
+
+    let client: S3CrtClient = get_test_client();
+    let result = client.head_object(&bucket, &key).await.expect("head_object failed");
+
+    assert_eq!(result.bucket, bucket);
+    assert_eq!(result.object.key, key);
+    assert!(!result.object.restored, "object should become restored only after restoration");
+
+    sdk_client
+        .restore_object()
+        .bucket(&bucket)
+        .key(&key)
+        .restore_request(aws_sdk_s3::model::RestoreRequest::builder()
+            .set_days(Some(1))
+            .set_glacier_job_parameters(Some(aws_sdk_s3::model::GlacierJobParameters::builder()
+                .set_tier(Some(aws_sdk_s3::model::Tier::Expedited))
+                .build()
+            ))
+            .build()
+        )
+        .send()
+        .await
+        .unwrap();
+
+    let timeout_s = 300;
+    let mut i = 0;
+    while i < timeout_s && !client.head_object(&bucket, &key).await.expect("head_object failed").object.restored {
+        println!("restoration is in progress, timeout in: {}s", timeout_s - i);
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        i += 1;
+    }
+    assert!(i < timeout_s, "timeouted while waiting for object become restored");
 }

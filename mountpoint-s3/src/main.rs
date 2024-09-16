93d4ddad90a0d206380f7d03837688913ecd4516
Monthon Klongklaew <monthonk@amazon.com>
2024-09-16 13:48:05 +0000

    mimalloc
    Signed-off-by: Monthon Klongklaew <monthonk@amazon.com>

--- mountpoint-s3/src/main.rs
+++ mountpoint-s3/src/main.rs
@@ -1,3 +1,8 @@
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 fn main() -> anyhow::Result<()> {
     mountpoint_s3::cli::main(mountpoint_s3::cli::create_s3_client)
 }
